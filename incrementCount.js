73332143ba1f01b5a1ce3dde72e57deae6909c36
Livia Lima <livia.n.lima@gmail.com>
2021-10-01 21:07:52 -0300

    new playlist + redesign in progress

--- incrementCount.js
+++
@@ -1,7 +0,0 @@
-function incrementCount() {
-	var d1 = document.getElementById('visits');
-	const visits = fetch('https://api.livialima.net/')
-		.then(res => res.json())
-		.then(data => d1.insertAdjacentText('beforeend', data))
-		.catch(error => console.log(error))
-}
